91121ea6978183a190a689eea713f4b71a868002
CloakerAlpha <yourhed@gmail.com>
2022-10-21 08:17:54 +0700

    3rd Commit
    *Redundant labels, hardcoded string, etc, will be optimized after all feature succesfully implemented
    
    6A. Added few image files
    7A. Added simple logic button on login page
    8A. Added fragments base code
    
    XX.More to add
    
    Note :
    N4. Enabled new plugin "ADB WIFI" for easier acces to debug for targeted device, ADVAN-GTab with 800 dp set.

--- app/src/main/java/com/smada/app/litelda/activities/LoginAct.kt
+++ app/src/main/java/com/smada/app/litelda/activities/LoginAct.kt
@@ -1,17 +1,32 @@
 package com.smada.app.litelda.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.core.view.WindowCompat
 import com.smada.app.litelda.R
+import com.smada.app.litelda.databinding.ActivityLoginBinding
 
-class LoginAct : AppCompatActivity() {
+class LoginAct : AppCompatActivity(), View.OnClickListener {
 
-    //private lateinit var binding: LoginActBinding
+    private lateinit var binding : ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding =ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btn_login -> {
+                val goToDashboard = Intent(this@LoginAct, DashboardAct::class.java)
+                startActivity(goToDashboard)
+            }
+        }
     }
 }
